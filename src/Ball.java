d67c7f53b8b2f112ebb0ad3920f9698ec1050558
paronim5 <pasha.kosov@gmail.com>
2024-05-17 19:36:03 +0200

    created constructor and getters and setters for class ball

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,65 @@
 public class Ball {
+    private int x;
+    private int y;
+    private int diameter;
+    private int speedX;
+    private int speedY;
+    private boolean moving;
+
+    public Ball(int x, int y, int diameter, int speedX, int speedY, boolean moving) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.moving = moving;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getSpeedX() {
+        return speedX;
+    }
+
+    public void setSpeedX(int speedX) {
+        this.speedX = speedX;
+    }
+
+    public int getSpeedY() {
+        return speedY;
+    }
+
+    public void setSpeedY(int speedY) {
+        this.speedY = speedY;
+    }
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
 }
--- src/BallMovementStrategy.java
+++ src/BallMovementStrategy.java
@@ -1,2 +1,11 @@
-public class  BallMovementStrategy implements Move{
+public class  BallMovementStrategy implements MovementStrategy{
+    @Override
+    public void move(Ball ball) {
+
+    }
+
+    @Override
+    public void moveP(Paddle paddle) {
+
+    }
 }
