fb25aa383e19e2fdeadd53e3e124e9387be313a7
paronim5 <pasha.kosov@gmail.com>
2024-05-18 22:18:23 +0200

    in class KetHandler create methods for moving paddle with A to the left and D to the right

--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -1,2 +1,37 @@
-public class KeyHandler {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public static boolean leftPressed;
+    public static boolean rightPressed;
+
+    public KeyHandler() {
+        leftPressed = false;
+        rightPressed = false;
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        if (keyCode == KeyEvent.VK_A) {
+            leftPressed = true;
+        } else if (keyCode == KeyEvent.VK_D) {
+            rightPressed = true;
+
+    }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            if (keyCode == KeyEvent.VK_A) {
+                leftPressed = false;
+            } else if (keyCode == KeyEvent.VK_D) {
+                rightPressed = false;
+    }
+    }
 }
