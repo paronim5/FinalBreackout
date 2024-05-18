e71a444e78ae2ddd36a02f1925948e73f3d8252f
paronim5 <pasha.kosov@gmail.com>
2024-05-18 23:15:07 +0200

    created method to check collision with ball and paddle

--- src/CollisionDetector.java
+++ src/CollisionDetector.java
@@ -1,2 +1,11 @@
+import java.awt.*;
+
 public class CollisionDetector {
+    public static boolean checkCollision(Ball ball, Paddle paddle) {
+        Rectangle ballRect = ball.hitBox();
+        Rectangle paddleRect = paddle.hitBox();
+        return ballRect.intersects(paddleRect);
+    }
+
+
 }
