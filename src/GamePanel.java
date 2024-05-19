9ddd1a4d86c869d4b33403787f5e0506f67f73c6
paronim5 <pasha.kosov@gmail.com>
2024-05-19 20:41:40 +0200

    created variables and getters and setters for class GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,4 +1,38 @@
 import javax.swing.*;
+import java.util.List;
 
-public class GamePanel {
+public class GamePanel extends JPanel {
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 800;
+    private Ball ball;
+    private Paddle paddle;
+    private List<Block> bricks;
+
+//region getters and setters
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Paddle getPaddle() {
+        return paddle;
+    }
+
+    public void setPaddle(Paddle paddle) {
+        this.paddle = paddle;
+    }
+
+    public List<Block> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Block> bricks) {
+        this.bricks = bricks;
+    }
+    //endregion
+
+
 }
