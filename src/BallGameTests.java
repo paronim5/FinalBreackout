28a41910d082f573107041c8bc2a0d4f5bfe2e93
paronim5 <pasha.kosov@gmail.com>
2024-05-26 21:54:50 +0200

    create BallGameTests class with test methods for class ball

---
+++ src/BallGameTests.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallGameTests {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void reverseX() {
+    }
+
+    @Test
+    void reverseY() {
+    }
+}
