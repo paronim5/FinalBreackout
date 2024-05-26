ac1909e86028e103567952da3d4efd1aa87fa0d1
paronim5 <pasha.kosov@gmail.com>
2024-05-26 22:01:27 +0200

    created method for testing reverseX method in Ball class

--- src/BallGameTests.java
+++ src/BallGameTests.java
@@ -4,14 +4,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BallGameTests {
+    private Ball ball;
+    private Paddle paddle;
+    private Block block;
 
     @BeforeEach
     void setUp() {
-
+        ball = new Ball(100, 100, 20, 2, 2, true);
+        paddle = new Paddle(95, 150, 50, 10, 5);
+        block = new Block(100, 50, 30, 10);
     }
 
     @Test
     void reverseX() {
+        ball.reverseX();
+        // check that the x direction has reversed
+        assertEquals(-2, ball.getSpeedX());
     }
 
     @Test
